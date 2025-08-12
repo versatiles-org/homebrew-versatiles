c26054a5ea5a98c3f767c88a11a61b0496268352
Michael Kreil <github@michael-kreil.de>
2025-08-12 23:08:42 +0000

    update formula automatically

--- Formula/versatiles.rb
+++ Formula/versatiles.rb
@@ -1,17 +1,17 @@
 class Versatiles < Formula
 	desc "A toolbox for converting, checking and serving map tiles in various formats."
 	homepage "https://github.com/versatiles-org/versatiles-rs"
-	version "0.15.6"
+	version "0.15.7"
 	license "MIT"
 
 	on_arm do
-		url "https://github.com/versatiles-org/versatiles-rs/releases/download/v0.15.6/versatiles-macos-aarch64.tar.gz"
-		sha256 "38b53f31dbb934e74773eb6ac9296cd08fda4c4d951b56197a8ccaea0e52ef5f"
+		url "https://github.com/versatiles-org/versatiles-rs/releases/download/v0.15.7/versatiles-macos-aarch64.tar.gz"
+		sha256 "615a3ebaa6b2a82261727824ae1d53f1042ef17c7544067802a176c76914ee8d"
 	end
 
 	on_intel do
-		url "https://github.com/versatiles-org/versatiles-rs/releases/download/v0.15.6/versatiles-macos-x86_64.tar.gz"
-		sha256 "cd5bc7ffab8cda26f52deb6b3e83c73f4a3fb52cc2f489b2026dd74ee9e9ae6d"
+		url "https://github.com/versatiles-org/versatiles-rs/releases/download/v0.15.7/versatiles-macos-x86_64.tar.gz"
+		sha256 "0bb791e8ebabaf943594df142b556e59f939c798aaf37607356d8d526ce94549"
 	end
 
 	def install
