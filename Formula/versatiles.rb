9de4d2bb2cb06966d7479a63e38829b892a2c899
Michael Kreil <github@michael-kreil.de>
2025-10-22 14:59:03 +0000

    update formula automatically

--- Formula/versatiles.rb
+++ Formula/versatiles.rb
@@ -6,12 +6,12 @@ class Versatiles < Formula
 
 	on_arm do
 		url "https://github.com/versatiles-org/versatiles-rs/releases/download/v1.1.0/versatiles-macos-aarch64.tar.gz"
-		sha256 "9655cb67b070ffcb692732bd872f1f5a8ecac0f67006ae63a655bd95f6306e38"
+		sha256 "9dae22910a8ccf8d88036c6e32be20f7f30af5ac17c1f3ce5e293852433ee64e"
 	end
 
 	on_intel do
 		url "https://github.com/versatiles-org/versatiles-rs/releases/download/v1.1.0/versatiles-macos-x86_64.tar.gz"
-		sha256 "ef35f7e388cee153a1f97b3a295c046bdc20e298fead32fe4258b170311c49a3"
+		sha256 "9ab4e08174dcda0220f9793be0fdf8b421489e5f72b626dbeb74e03fb971ee4c"
 	end
 
 	def install
